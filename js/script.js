1a5c4e43d2173f55fec0379e1f219e53e7693fb6
Kajetan Kuzma <kajetankuzma@gmail.com>
2021-01-17 21:39:40 +0100

    Testing background image displaying when smartphone keyboard is shown.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,10 @@ document.addEventListener("DOMContentLoaded", appStart)
 //FUNKCJA STARTOWA ................................................................................
 function appStart()
 {   
+
+    setPixelSize();
+
+
     const myApiKey = config.MY_KEY;
     const weatherApi =
         {
@@ -50,6 +54,15 @@ function appStart()
         {
             date.innerText = day + "/" + month + "/" + year;
         }
+    }
+    function setPixelSize()
+    {
+        const screenWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+        const screenHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+        
+        const tagBody = document.body;
+        tagBody.style.width = screenWidth;
+        tagBody.style.height = screenHeight;
     }
 }
 
